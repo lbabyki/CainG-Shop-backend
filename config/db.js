aae7547aa7ffda8a1e5a63599f0c428013c8c4ac
caing <crglh25@gmail.com>
2025-05-25 18:07:20 +0700

    push

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URI);
     console.log("Connected");
   } catch (err) {
     console.log("Error Connect to db", err);
